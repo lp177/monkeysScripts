c88484bc849122e8c44ebe76235d243f5e268d5a
lp177 <lucperez177@gmail.com>
2019-11-03 09:23:15 +0100

    Update / clean up

--- Youtube/HideClarifyBoxOfYoutube.js
+++ Youtube/HideClarifyBoxOfYoutube.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name            HideClarifyBoxOfYoutube
-// @description     try to take over the world!
-// @version         1.3212
-// @grant           none
-// @include         /^http(s)?://(www\.)?youtube\.com/.+$/
+// @name            Youtube - HideClarifyBox
+// @description     Hide clarify box below the video on Youtube
 // @author          lp177
-// @namespace       https://raw.githubusercontent.com/lp177/monkeysScripts/master/README.md
-// @downloadURL     https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/HideClarifyBoxOfYoutube.js
-// @updateURL       https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/HideClarifyBoxOfYoutube.js
+// @namespace       lp177
+// @version         1.3213
+// @include         /^http(s)?://(www\.)?youtube\.com/.+$/
+// @grant           none
+// @downloadURL     https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/HideClarifyBox.js
+// @updateURL       https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/HideClarifyBox.js
 // ==/UserScript==
 
 function hide()
 {
-    document.querySelector( '#clarify-box' ).style = 'display: none !important';
+	if ( document.querySelector( '#clarify-box' ) )
+    	document.querySelector( '#clarify-box' ).style = 'display: none !important';
 }
 
 // content reloaded in ajax ?
