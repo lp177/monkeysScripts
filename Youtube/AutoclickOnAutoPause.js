cb7dd5172055f46e5c09dd9e969c919d4da4ef63
lp177 <lucperez177@gmail.com>
2020-03-30 14:03:11 +0200

    Do not cut my music bro ! ><'

---
+++ Youtube/AutoclickOnAutoPause.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name            Youtube - Auto click on auto pause
+// @description     Click on the confirm button to unpause the automatic paused video for long time opening window on Youtube
+// @namespace    	lp177
+// @author       	lp177
+// @version         1.0001
+// @include         /^http(s)?://(www\.)?youtube\.com/.+$/
+// @grant        	none
+// @downloadURL     https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/AutoclickOnAutoPause.js
+// @updateURL       https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/AutoclickOnAutoPause.js
+// ==/UserScript==
+
+function hide()
+{
+	if ( document.querySelector( '#confirm-button.yt-confirm-dialog-renderer #button' ) )
+	    document.querySelector("#confirm-button.yt-confirm-dialog-renderer #button").click()
+}
+
+setTimeout( function(){ setInterval(hide, 600000) }, 1000); // Launched only after 10mn useless previous that minimal time
