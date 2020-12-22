a4020c1e335b68fbb9be64fb48b8a459c9e262a8
lp177 <lucperez177@gmail.com>
2020-12-22 06:55:51 +0100

    Update RGPD comfort rules

--- Global/delete_consent_tracking_popup.js
+++ Global/delete_consent_tracking_popup.js
@@ -48,7 +48,7 @@
 		if (removePopUp('#onetrust-consent-sdk')) return outputDebug( '#onetrust-consent-sd', verbose);
 		else if (removePopUp('.cookie-banner-layer')) return outputDebug( '.cookie-banner-layer', verbose);
 		else if (removePopUp('#didomi-host', 'didomi-popup-open')) return outputDebug( '#didomi-host', verbose);
-		else if (removePopUp('#iubenda-cs-banner')) return outputDebug( '#iubenda-cs-banner', verbose);
+		else if (removePopUp('#iubenda-cs-banner',null,null,'overflow: auto;')) return outputDebug( '#iubenda-cs-banner', verbose);
 		else if (removePopUp('div[id^="sp_message_container_"]')) return outputDebug( 'div[id^="sp_message_container_"]', verbose);
 		else if (removePopUp('body[style="overflow: hidden;"] div[role="presentation"],body[style="overflow: auto;clear177:true;"] div[role="presentation"]', null, 'overflow: auto;clear177:true;')) return outputDebug( 'body[style="overflow: hidden;"] div[role="presentation"]', verbose);
 		else if (removePopUp('.truste_overlay[id^="pop-div"]') && removePopUp('.truste_box_overlay[id^="pop-div"]')) return outputDebug( '.truste_overlay[id^="pop-div"]', verbose);
