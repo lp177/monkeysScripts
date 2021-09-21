9b80ca5a2d8e213c8399ea258e99a2713d92f790
lp177 <lucperez177@gmail.com>
2021-09-21 19:19:26 +0200

    timers in bad order

--- Youtube/AutoclickOnAutoPause.js
+++ Youtube/AutoclickOnAutoPause.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @version         1.0003
 // @name            Youtube - Auto click on auto pause
 // @description     Click on the confirm button to unpause the automatic paused video for long time opening window on Youtube
-// @include         /^http(s)?://(www\.)?youtube\.com/*
-// @grant        	none
 // @namespace    	lp177
 // @author       	lp177
+// @version         1.0003
+// @include         /^http(s)?://(www\.)?youtube\.com/*
+// @grant        	none
 // @icon         	https://www.google.com/s2/favicons?domain=www.youtube.com
 // @downloadURL     https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/AutoclickOnAutoPause.js
 // @updateURL       https://raw.githubusercontent.com/lp177/monkeysScripts/master/Youtube/AutoclickOnAutoPause.js
@@ -17,4 +17,4 @@ function hide()
 	    document.querySelector("#confirm-button.yt-confirm-dialog-renderer #button").click()
 }
 
-setTimeout( function(){ setInterval(hide, 600000) }, 1000); // Launched only after 10mn useless previous that minimal time
+setTimeout( function(){ setInterval(hide, 1000) }, 600000); // Launched only after 10mn useless previous that minimal time
