455e3870aaa8e8e509020fc49539e9c0a442dc21
lp177 <lucperez177@gmail.com>
2021-10-20 15:22:37 +0200

    Update default keywords

--- GoogleNews/removeUnwantedShit.js
+++ GoogleNews/removeUnwantedShit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google news - Remove clickbait by forbidden keyword in tittle, and remove unwanted section by name
 // @namespace    http://tampermonkey.net/
-// @version      0.0002
+// @version      0.0003
 // @description  Remove google news articles where title match on forbidden list of words and sections of articles who matching with unwantedSections list of names
 // @author       lp177
 // @match        https://news.google.com/*
@@ -13,13 +13,19 @@
     'use strict';
 
 	const unwantedSections = ['Sports','Divertissement','Économie','Pour vous'];
-	const forbiddensKeywords = ['disparition', 'disparue', 'décèdé', 'décède', 'obsèque', 'tpmp','samsung','xiaomi','cac 40'];
+	const forbiddensKeywords = [
+		'disparition','disparue','enlèvement','sondage',
+		'présidentielle 2022','election présidentielle','darmanin','douard philippe','procès des attentats du 13 Novembre',
+		'décèdé','décède','décédé','popularité','conjoint','extorqué','meurtre','agression','tue','obsèque','inceste',
+		'tpmp','samsung','xiaomi','cac 40','iphone',
+		'viol','drame','hommage','manuel valls','blanquer','véran ','castex','gabriel attal','zemmour'
+	];
 
 	function filterArticles()
 	{
 		for (let title of document.querySelectorAll('article > h3 > a'))
 		{
-			if ( forbiddensKeywords.some( v => title.innerText.toLowerCase().includes(v) ) )
+			if ( forbiddensKeywords.some( v => title.innerText.toLowerCase().includes(v.toLowerCase()) ) )
 			{
 				console.info('Removed: ', title.innerText, title.parentNode.parentNode.parentNode.parentNode);
 				title.parentNode.parentNode.parentNode.parentNode.remove();
@@ -41,8 +47,14 @@
 				targetedSectionStart.nextSibling.remove();
 		}
 	}
+	function removeSideSugestions()
+	{
+		document.querySelectorAll('aside article').forEach((item)=>item.remove());
+	}
 	filterArticles();
 	removeSection();
+	removeSideSugestions();
 	setInterval( filterArticles, 1000 );
 	setInterval( removeSection, 1000 );
+	setInterval( removeSideSugestions, 1000 );
 })();
