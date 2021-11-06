422fd60c663182067d302dbee7e9140a6fb46b98
lp177 <lucperez177@gmail.com>
2021-11-06 19:14:00 +0100

    Make more focus on important elements

--- Uber/focus_tracking_map.js
+++ Uber/focus_tracking_map.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @version      0.0002
-// @name         Ubereat - Focus tracking map, remove useless UI
+// @name         Ubereat - Focus traking map, remove useless UI
 // @namespace    lp177
 // @author       lp177
-// @description  Remove header/footer for use as fullscreen tracking map on tracking order page
+// @description  Remove header/footer for use as fullscreen tracking map on traking order page
 // @match        https://www.ubereats.com/fr/orders/*
 // @match        https://www.ubereats.com/orders/*
 // @icon         https://www.google.com/s2/favicons?domain=www.ubereats.com
@@ -26,8 +26,18 @@
 				document.querySelector('header').remove();
 			if (document.querySelector('#main-content > div:last-of-type > div:last-of-type'))
 				document.querySelector('#main-content > div:last-of-type > div:last-of-type').setAttribute('style','top:0!important;bottom:0!important;');
+			if(document.querySelector('#main-content > div'))
+				document.querySelector('#main-content > div').setAttribute('style','max-width:250px;');
 		}
 	}
+    document.querySelector('head').insertAdjacentHTML(
+		'beforeend',
+		`<style type="text/css" id="injected177">
+		.cw{box-shadow: 0 0 50px 10px red !important;}
+		.f1,.f2,.f3,.f4{box-shadow: 0 0 20px 1px blue !important;background-color:rgba(0,0,250,0.1) !important;border-radius:25%;}
+		.fg{box-shadow: 0 0 50px 10px green !important;}
+		</style>`
+	)
 	document.querySelector('body').setAttribute('style', 'overflow:hidden');
 	update();
 	window.setTimeout(update, 300);
