21a4bf61e5d20622a7396478e356c9353ecda5ab
lp177 <lucperez177@gmail.com>
2022-05-30 20:10:28 +0200

    Add capability to use subtabs without automatic focus

--- Twitch/video.js
+++ Twitch/video.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.001
+// @version      1.000
 // @name         Twitch - Somes fix on this ****ing video player
 // @description  stop to autoplay anything and add play / pause on click in video block like any normal web video player... Click on timed bonus (icon of chest at left of chat input) automaticaly.
 // @author       lp177
@@ -14,7 +14,7 @@
 (function() {
     'use strict';
 	var debugIsFun=()=>null;
-	var stats={savedUrl:null,urlChanged:false,uiSwitched:false};
+	var stats={savedUrl:null,urlChanged:false,uiSwitched:false,switcherPaused:false};
 	// debugIsFun=console.info;
 	function pauseOrDie(e,remaining_try=5)
 	{
@@ -26,25 +26,40 @@
 	}
 	function pauseAutoPlay(e)
 	{
-			debugIsFun('video presumed auto played: ', e.target);
-			setTimeout(()=>pauseOrDie(e),10);
-			e.target.removeEventListener('play',pauseAutoPlay);
+		debugIsFun('video presumed auto played: ', e.target);
+		setTimeout(()=>pauseOrDie(e),10);
+		e.target.removeEventListener('play',pauseAutoPlay);
+	}
+	function doNotStartMuted(retry=0)
+	{
+		if(retry>5)return;
+		const muttedButton=document.querySelector('button[aria-label="Réactiver l\'audio (M)"]');
+		if(muttedButton)
+			muttedButton.click();
+		setTimeout(()=>doNotStartMuted(retry+1),500);
 	}
 	function updateVideos()
 	{
+		if(stats.switcherPaused)return;
 		if(window.location.href!==stats.savedUrl)
 		{
 			stats.savedUrl=window.location.href;
 			stats.uiSwitched=false;
+			document.querySelector('a>.tw-title')&&document.querySelector('a>.tw-title').addEventListener('click',()=>{
+				stats.switcherPaused=true;
+				setTimeout(()=>stats.switcherPaused=false,30000);
+			});
 		}
 		if(document.querySelector('.community-points-summary .claimable-bonus__icon'))// Click on chest button for collect points
 			document.querySelector('.community-points-summary .claimable-bonus__icon').click();
+		if(document.querySelector('a[data-a-target="hosting-indicator"]'))// Open real url after hosting
+			document.querySelector('a[data-a-target="hosting-indicator"]').click();
 		if(!stats.uiSwitched)
 		{// Open detail view of current video for have full and generic ui
 			stats.uiSwitched=true;
 			if(document.querySelector('.channel-status-info--live'))// We are on live streaming
 			{
-				if(window.location.href.split('#')[0].split('?')[0].split('/').length===4)// Avoid to lock usage of others tabs
+				if([4,5].includes(window.location.href.split('#')[0].split('?')[0].split('/').length))// Avoid to lock usage of others tabs
 					document.querySelector('.channel-status-info--live').click();
 			}
 			else if(document.querySelector('div[data-a-player-type="channel_home_carousel"]'))// We are on replay video
@@ -58,6 +73,7 @@
 		v.classList.add('updatedBy177');
 		v.addEventListener('click',v.pause);
 		v.addEventListener('play',pauseAutoPlay);
+		setTimeout(doNotStartMuted,500);
 		debugIsFun('UpdatedBy177/', v);
 		const overlay=document.querySelector('.video-player__overlay');
 		if(overlay)
