89ee89590f179ef7c6c25714a434f172f5ae2c1e
lp177 <lucperez177@gmail.com>
2022-06-22 10:53:25 +0200

    Update for new version of google news

--- GoogleNews/hideUnwantedInformationsAbout.js
+++ GoogleNews/hideUnwantedInformationsAbout.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      0.0004
+// @version      0.0005
 // @name         Google news - Remove clickbait by forbidden keyword in tittle, and remove unwanted section by name
 // @description  Remove google news articles where title match on forbidden list of words and sections of articles who matching with unwantedSections list of names
 // @namespace    lp177
@@ -25,11 +25,13 @@
 
 	function filterArticles()
 	{
-		for (let title of document.querySelectorAll('article > h3 > a'))
+		var title_txt;
+		for (let title of document.querySelectorAll('article a'))
 		{
-			if ( forbiddensKeywords.some(v=>title.innerText.toLowerCase().includes(v.toLowerCase())))
+			title_txt=title.hasAttribute('aria-label')?title.getAttribute('aria-label'):title.innerText;
+			if (forbiddensKeywords.some(v=>title_txt.toLowerCase().includes(v.toLowerCase())))
 			{
-				console.info('Removed: ', title.innerText, title.parentNode.parentNode.parentNode.parentNode);
+				console.info('Removed: ', title_txt, title.parentNode.parentNode.parentNode.parentNode);
 				title.parentNode.parentNode.parentNode.parentNode.remove();
 			}
 		}
@@ -38,18 +40,23 @@
 	{
 		for (const sectionName of unwantedSections)
 		{
-			let targetedSectionTitle = document.evaluate("//h2/span/a[text()='"+sectionName+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+			let targetedSectionTitle = document.evaluate(".//h2/span/a[text()='"+sectionName+"']|.//c-wiz//h3/a[text()='"+sectionName+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
 			if (!targetedSectionTitle)
 			{
 				console.log('Section ', sectionName, 'Not found');
 				continue;
 			}
-
-			let targetedSectionStart = document.evaluate("//h2/span/a[text()='"+sectionName+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentNode.parentNode.parentNode.parentNode;
-
+			let targetedSectionStart = document.evaluate(".//c-wiz//h3/a[text()='"+sectionName+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+			if(targetedSectionStart)
+			{
+				targetedSectionStart.singleNodeValue.parentNode.parentNode.parentNode.parentNode.remove();
+				continue;
+			}
+			targetedSectionStart = document.evaluate(".//h2/span/a[text()='"+sectionName+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.parentNode.parentNode.parentNode.parentNode;
 			while (targetedSectionStart.nextSibling && !targetedSectionStart.nextSibling.querySelector('h2'))
 				targetedSectionStart.nextSibling.remove();
+
 		}
 	}
 	function removeSideSugestions()
