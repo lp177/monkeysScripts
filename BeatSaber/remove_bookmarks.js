c053100447503b683555adb8a799be0905781e72
lp177 <lucperez177@gmail.com>
2022-10-02 04:03:18 +0200

    Add script for baet saber

---
+++ BeatSaber/remove_bookmarks.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @version      0.0001
+// @name         BeastSaber - Clear bookmarks
+// @description  Remove all your bookmarks when you visit your bookmarks page
+// @namespace    lp177
+// @author       lp177
+// @match        https://bsaber.com/songs/new/?bookmarked_by=*
+// @match        https://bsaber.com/songs/new/page/*/?bookmarked_by=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bsaber.com
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	var found=0;
+	function searchAndDestroy(max_retry=20)
+	{
+		const buttons = document.querySelectorAll('.fas.fa-bookmark');
+		if(buttons.length<1&&--max_retry>0)
+		{
+			console.info('Wait for destruction...');
+			return setTimeout(()=>searchAndDestroy(max_retry),100);
+		}
+		found+=buttons.length;
+		for (let bt of buttons)
+		{
+			console.info('Destruction!!!');
+			bt.click();
+		}
+		setTimeout(()=>searchAndDestroy(max_retry-1),100);
+	}
+	setTimeout(searchAndDestroy,1500);
+	setTimeout(()=>{
+		if(found>0)location.reload();
+	},3100);
+})();
