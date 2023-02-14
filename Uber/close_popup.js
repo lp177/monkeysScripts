9ab48d870db3db04db4d21dbb848f741a09e2ac3
lp177 <lucperez177@gmail.com>
2023-02-14 12:29:02 +0100

    Enlarge selector

--- Uber/close_popup.js
+++ Uber/close_popup.js
@@ -1,22 +1,21 @@
 // ==UserScript==
 // @name         Ubereat - Close "how was your order?" popup automaticaly
 // @description  Close annoying popup on ubereat who appear after each order "how was your order?"
-// @version      0.003
+// @version      0.002
 // @namespace    lp177
 // @author       lp177
-// @match        https://www.ubereats.com/feed*
+// @match        https://www.ubereats.com/*
 // @icon         https://www.google.com/s2/favicons?domain=ubereats.com
 // @downloadURL  https://raw.githubusercontent.com/lp177/monkeysScripts/master/Uber/close_popup.js
 // @updateURL    https://raw.githubusercontent.com/lp177/monkeysScripts/master/Uber/close_popup.js
 // @grant        none
 // ==/UserScript==
-
 (function() {
     'use strict';
 	function skipPopUp()
 	{
 		tryCount++;
-		if (!document.querySelector('#wrapper footer + div button'))
+		if (!document.querySelector('#wrapper footer + div button, div[data-test="desktop-dialog"] button'))
 		{
 			if (intervalId!=null&&tryCount>100)
 				clearInterval(intervalId)
@@ -24,7 +23,7 @@
 		}
 		if (intervalId!=null)
 			clearInterval(intervalId)
-		document.querySelector('#wrapper footer + div button').click();
+		document.querySelector('#wrapper footer + div button, div[data-test="desktop-dialog"] button').click();
 	}
 	var intervalId = null,tryCount=0;
 	skipPopUp();
