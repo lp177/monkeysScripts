c1eb54661f5d7be66ca6188e51ac7cd72c3b3a5a
lp177 <lucperez177@gmail.com>
2023-10-18 08:13:43 +0200

    Add shortcuts to js console

---
+++ Global/console_toolbox.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @version      0.0001
+// @name         Console toolbox
+// @description  Add tools and shortcuts in your browser's console
+// @namespace    lp177
+// @author       lp177
+// @match        http://*/*
+// @match        https://*/*
+// @exclude         https://*.google.com/*
+// @grant        none
+// @noframes
+// @downloadURL  https://raw.githubusercontent.com/lp177/monkeysScripts/master/Global/console_toolbox.js
+// @updateURL    https://raw.githubusercontent.com/lp177/monkeysScripts/master/Global/console_toolbox.js
+// ==/UserScript==
+window.q=document.querySelector.bind(document);
+window.qa=document.querySelectorAll.bind(document);
+window.cl=console.log.bind(console);
+window.ci=console.info.bind(console);
+window.ce=console.error.bind(console);
