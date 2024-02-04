3f85a7ccbe5cff1aabc32532a7172ee7839ed07e
lp177 <lucperez177@gmail.com>
2024-02-04 15:46:13 +0100

    Fix bug causing automatic closing menu of quantity

--- Amazon/switchSortCommentsToMostRecents.js
+++ Amazon/switchSortCommentsToMostRecents.js
@@ -3,7 +3,7 @@
 // @description  	Select by default most recents comments sorting until of best ratings
 // @author          lp177
 // @namespace       lp177
-// @version      	1.0001
+// @version      	1.0002
 // @match        	https://www.amazon.fr/*
 // @match        	https://www.amazon.com/*
 // @grant        	none
@@ -14,12 +14,20 @@
 
 (function() {
     'use strict';
+	function isVisible(el)
+	{
+		var rect = el.getBoundingClientRect();
+		var elemTop = rect.top;
+		var elemBottom = rect.bottom;
+		return (elemTop >= 0) && (elemBottom <= window.innerHeight);
+	}
     function switchToMostRecent()
 	{
-		if (document.querySelector('#cm-cr-dp-review-sort-type span span select'))
+		var select = document.querySelector('#cm-cr-dp-review-sort-type span span select');
+		if (isVisible(select))
 		{
 			if (document.querySelector('#cm-cr-sort-dropdown[aria-pressed="false"],#cm-cr-sort-dropdown:not([aria-pressed])'))
-				document.querySelector('#cm-cr-dp-review-sort-type span span select').click();
+				select.click();
 			if(document.querySelector(`a[data-value='{"stringVal":"recent"}']`))
 				return document.querySelector(`a[data-value='{"stringVal":"recent"}']`).click();
 		}
