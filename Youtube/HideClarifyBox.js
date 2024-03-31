8f5706daef7afed067489256c10883a879d06c29
lp177 <lucperez177@gmail.com>
2024-03-31 17:48:22 +0200

    Remove new banner

--- Youtube/HideClarifyBox.js
+++ Youtube/HideClarifyBox.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version         1.3215
+// @version         1.3216
 // @name            Youtube - HideClarifyBox
 // @description     Hide clarify box below the video on Youtube
 // @include         /^http(s)?://(www\.)?youtube\.com/*
@@ -13,11 +13,12 @@
 
 function hide()
 {
-	if ( document.querySelector( '#clarify-box' ) )
-    	document.querySelector( '#clarify-box' ).style = 'display: none !important';
+	if (document.querySelector('#clarify-box'))
+    	document.querySelector('#clarify-box').style = 'display: none !important';
+	if (document.querySelector('ytd-info-panel-content-renderer'))
+    	document.querySelector('ytd-info-panel-content-renderer').style = 'display: none !important';
 }
 
 // content reloaded in ajax ?
 setInterval(hide, 20000);
-
 setTimeout( hide, 1000);
