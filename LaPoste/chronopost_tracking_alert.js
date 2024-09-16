6d1449ad167c1dfd9631f092809d63a42a516245
lp177 <lucperez177@gmail.com>
2024-09-16 13:48:56 +0200

    Fix on text targeted for alert of changes

--- LaPoste/chronopost_tracking_alert.js
+++ LaPoste/chronopost_tracking_alert.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      0.1205
+// @version      0.1206
 // @name         Chronopost - Alert on event trackin
 // @description  Display notification on each delivery status changeg for chronopost
 // @author       lp177
@@ -14,7 +14,7 @@
 (function() {
     'use strict';
 
-    const timeUntilTwoCheck = 10;
+    const timeUntilTwoCheck = 30;
 
     async function alertOnChange() {
         if (!document.querySelector('#numeroLT') || !document.querySelector('#numeroLT').value)
@@ -22,26 +22,27 @@
 
         const track_id = document.querySelector('#numeroLT').value;
         const last_change_in_history = localStorage.getItem('chronopost_alert_on_change.' + track_id);
+		const lastText=document.querySelector('#tracking-container tbody tr td:nth-of-type(2)')?.innerText;
 
         if (last_change_in_history === null)
 		{
             localStorage.setItem(
 				'chronopost_alert_on_change.' + track_id,
-				document.querySelector('#suiviTab tbody tr:nth-of-type(2) td').innerText
+				lastText
 			);
             return setTimeout(location.reload.bind(window.location), timeUntilTwoCheck * 1000); //First load - Init history for next comparaison
         }
-        if (last_change_in_history === document.querySelector('#suiviTab tbody tr:nth-of-type(2) td').innerText)
+        if (last_change_in_history === lastText)
             return setTimeout(location.reload.bind(window.location), timeUntilTwoCheck * 1000); //No change until last alert / first load
 
         // New event detected !
         localStorage.setItem(
 			'chronopost_alert_on_change.' + track_id,
-			document.querySelector('#suiviTab tbody tr:nth-of-type(2) td').innerText
+			lastText
 		);
 		GM_notification({
 		  title: "LaPoste Traking",
-		  text: document.querySelector('#suiviTab tbody tr:nth-of-type(2) td:nth-of-type(2)').innerText,
+		  text: lastText,
 		  url: location.href
 		});
 		setTimeout(location.reload.bind(window.location), timeUntilTwoCheck * 1000);
