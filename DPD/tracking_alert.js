b40993d00d6bb228c66d703003f3a49c697be12f
lp177 <lucperez177@gmail.com>
2025-03-25 14:33:08 +0100

    Add history cleaner

--- DPD/tracking_alert.js
+++ DPD/tracking_alert.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version      1.0000.001
+// @version      1.1001.001
 // @name         DPD - Notify on status change
 // @description  try to take over the world!
 // @author       lp177
@@ -14,8 +14,20 @@
     'use strict';
     const timeUntilTwoCheck = 30;
 	const notification_name = 'DPD';
-	const get_track_id = ()=>document.querySelector('.parcelAlias')?.innerText.trim();
-	const get_status = ()=>document.querySelector('.parcelAlias+span+span.gray-out')?.innerText.trim();
+	const get_track_id = () => new URLSearchParams(document.location.search).get("parcelNumber")?.trim();
+	const get_status = () => document.querySelector('.parcelAlias+span+span.gray-out')?.innerText.trim();
+	function remove_outdated_history()
+	{
+		for (const key of Object.keys(localStorage))
+		{
+			if (!key.startsWith(notification_name + '_alert_on_change.'))
+				continue;
+			const timestamp_of_last_change = parseInt(localStorage.getItem(key).split(';', 1));
+			const days_from_last_change = (Date.now() - timestamp_of_last_change) / 1000 / 60 / 60 / 24;
+			if (days_from_last_change > 30)
+				localStorage.removeItem(key);
+		}
+	}
     async function alertOnChange() {
         const track_id = get_track_id();
         if (!track_id)
@@ -23,22 +35,23 @@
 			setTimeout(alertOnChange, 1000);
             return console.info('Nothing to track'); // Nothing to track avoid to refresh without track id
 		}
-        const last_change_in_history = localStorage.getItem(notification_name + '_alert_on_change.' + track_id);
+        let last_change_in_history = localStorage.getItem(notification_name + '_alert_on_change.' + track_id);
         const lastText = get_status();
         if (last_change_in_history === null) // First load - Init history for next comparaison
         {
             localStorage.setItem(
                 notification_name + '_alert_on_change.' + track_id,
-                lastText
+                String(Date.now()) + ';' + lastText
             );
             return;
         }
+		last_change_in_history = last_change_in_history.substr(last_change_in_history.split(';', 1)[0].length+1); // Remove timestamp part
         if (last_change_in_history === lastText) //No change until last alert / first load
             return;
         // New status detected !
         localStorage.setItem(
             notification_name + '_alert_on_change.' + track_id,
-            lastText
+            String(Date.now()) + ';' + lastText
         );
         GM_notification({
           title: notification_name + " Traking",
@@ -48,4 +61,5 @@
     }
     setTimeout(alertOnChange, 1000);
 	setTimeout(()=>get_track_id()&&location.reload.bind(window.location), timeUntilTwoCheck * 1000);
+	remove_outdated_history();
 })();
